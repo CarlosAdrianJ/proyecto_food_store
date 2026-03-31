7fcd2af7e61f0254611e49a0bb0690f8fdf51f58
CarlosAdrianJ <aslak71@hotmail.com>
2026-03-31 07:46:00 -0300

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <section id="center">
+        <div className="hero">
+          <div className="hero__content">
+            <h1 className="hero__title">proyecto base levantado correctamente</h1>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default App
